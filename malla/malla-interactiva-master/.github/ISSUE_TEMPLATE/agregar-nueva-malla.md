78abdcff22c4d210ea23d3d9b1ed755b6c46e24f
VicariousMoth <68717483+VicariousMoth@users.noreply.github.com>
2025-09-25 12:04:15 -0300

    si

---
+++ malla/malla-interactiva-master/.github/ISSUE_TEMPLATE/agregar-nueva-malla.md
@@ -0,0 +1,19 @@
+---
+name: Agregar nueva Malla
+about: Dar aviso e información sobre una malla que no se encuentre en el projecto
+title: Quiero agregar una Malla
+labels: mallas
+assignees: ''
+
+---
+> Por favor borrar todo texto antecedido por ">"
+
+
+**Nombre carrera**
+> escribir carrera
+
+**Abreviatura de la carrera**
+> por ejemplo, INF (Informática)
+
+**Información de la malla**
+> Insertar links o imagenes que muestren información de la nueva malla
---
+++ malla/malla-interactiva-master/scripts/minify_code.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# based on:
+# https://unix.stackexchange.com/questions/249701/how-to-minify-javascript-and-css-with-command-line-using-minify-tool
+# minification of js files
+
+./scripts/minify_dev.sh
+npx terser js/init.js -c -m -o js/init.js
+find js/ -regex '.*[^(?:min[0-9+)|(init)]\.js' \
+  -delete
+
+# minification of css files
+find css/ -type f \
+    -name "*.css" ! -name "*.min.*" \
+    -exec echo {} \; \
+    -exec npx csso --input {} --output {}.min \; \
+    -exec rm {} \; \
+    -exec mv -f {}.min {} \;
+
+# compress js files even more with gzip
+gzip js/*
+
+# Create file representing last update date
+date +"%s000" | tee date.txt
+
+# moves assets to root
+cp ./assets/* ./
